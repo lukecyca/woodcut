a5d2adba5fed5df09eb1f322eb9af35dce002206
Luke Cyca <me@lukecyca.com>
2011-01-07 21:32:17 -0800

    Refactored path logic in generate methods.
    Created an relative_path() helper function for referencing relative files in templates which end up building files in multiple directories.
    
    Changed the words "make" to "build"

--- woodcut/project.py
+++ woodcut/project.py
@@ -13,78 +13,70 @@ IGNORE_FILES = ['.DS_Store']
 IGNORE_DIRECTORIES = ['templates']
 
 class Project(object):
-    def __init__(self, src_path='', build_path=''):
-        self.src_path = os.path.abspath(src_path)
-        self.build_path = os.path.abspath(build_path)
+    def __init__(self, src_root='', build_root=''):
+        self.src_root = os.path.abspath(src_root)
+        self.build_root = os.path.abspath(build_root)
     
-    def generate_file(self, src, dest):
-        ext = os.path.splitext(src)[1]
+    def build_file(self, root_relative_src_path):
+        def relative_path(path):
+            """Translates a root-relative path to a path relative to this file"""
+            backout_path = os.path.relpath('.', os.path.dirname(root_relative_src_path))
+            return os.path.normpath(os.path.join(backout_path, path))
+        
+        # Absolute paths to source and build objects
+        src_path = os.path.normpath(os.path.join(self.src_root, root_relative_src_path))
+        build_path = os.path.normpath(os.path.join(self.build_root, root_relative_src_path))
+        
+        extension = os.path.splitext(src_path)[1]
         
         # Skip non-templates, and just link them to the source file
-        if ext not in ['.xhtml', '.newtxt', '.xml']:
-            if os.path.exists(dest):
-                os.unlink(dest)
-            os.symlink(src, dest)
+        if extension not in ['.xhtml', '.newtxt', '.xml']:
+            if os.path.exists(build_path):
+                os.unlink(build_path)
+            os.symlink(src_path, build_path)
             return
         
         # Determine output filename
-        if ext == '.xhtml':
-            dest = os.path.splitext(dest)[0] + '.html'
+        if extension == '.xhtml':
+            build_path = os.path.splitext(build_path)[0] + '.html'
         else:
-            dest = os.path.splitext(dest)[0]
+            build_path = os.path.splitext(build_path)[0]
         
         # Render the template to the output path
-        print "Rendering %s" % os.path.basename(dest)
+        print "Rendering %s" % os.path.basename(build_path)
         template = None
-        title = None
-        if ext == '.newtxt':
-            with open(src) as f:
+        meta = {}
+        if extension == '.newtxt':
+            with open(src_path) as f:
                 template = NewTextTemplate(f)
         else:
-            template_loader = TemplateLoader('.', auto_reload=True)
-            template = template_loader.load(src)
+            template_loader = TemplateLoader(self.src_root, auto_reload=True)
+            template = template_loader.load(src_path)
             sp = SoupyPage()
-            sp.open(src)
-            title = SoupyPage.get_first(sp.get_meta('title'))
-        stream = template.generate(meta={'title': title, 'path_to_root' : ''})
-        fh = open(dest, 'w')
-        if ext == '.xhtml':
+            sp.open(src_path)
+            meta['title'] = SoupyPage.get_first(sp.get_meta('title'))
+        stream = template.generate(meta=meta, relative_path=relative_path)
+        fh = open(build_path, 'w')
+        if extension == '.xhtml':
             fh.write(stream.render('xhtml', doctype='xhtml'))
         else:
             fh.write(stream.render())
         fh.close()
-        
-        
-    def generate(self):
-        os.chdir(self.src_path)
+
+    def build(self):
+        os.chdir(self.src_root)
         for path, dirs, files in os.walk('.'):
             for d in dirs:
                 if d in IGNORE_DIRECTORIES:
                     dirs.remove(d)
-                elif not os.path.exists(os.path.join(self.build_path, path, d)):
-                    os.mkdir(os.path.join(self.build_path, path, d))
+                elif not os.path.exists(os.path.join(self.build_root, path, d)):
+                    os.mkdir(os.path.join(self.build_root, path, d))
             for f in [x for x in files if x not in IGNORE_FILES]:
-                self.generate_file(os.path.normpath(os.path.join(self.src_path, path, f)),
-                                   os.path.normpath(os.path.join(self.build_path, path, f)) )
-        
-        
+                self.build_file(os.path.join(path, f))
 
-
-#    def get_template_root(self):
-#        return os.path.join(self.src_path, 'templates')
-#        
-#    def get_template_mtime(self):
-#        """Returns the last time the templates were modified"""
-#        most_recent = os.getmtime(self.get_template_root())
-#        for root, dirs, files in os.walk(self.get_template_root()):
-#            for f in files:
-#                if f.endswith('.xhtml'):
-#                    most_recent = max(most_recent,getmtime(join(root,f)))
-#        return most_recent
-                           
     def clean(self):
         """Delete everything in the build directory"""
-        for root, dirs, files in os.walk(self.build_path, topdown=False):
+        for root, dirs, files in os.walk(self.build_root, topdown=False):
             for f in files:
                 os.remove(os.path.join(root,f))
             for d in dirs:
