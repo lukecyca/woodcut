74b9b4da1b1af51a8368a39e02509ab880a41947
Luke Cyca <me@lukecyca.com>
2011-12-23 11:16:53 -0800

    Updated setup.py

--- README.markdown
+++ README.markdown
@@ -1,4 +1,6 @@
 # Woodcut #
 *Minimalist content management system for static websites*
 
-<http://wiki.github.com/lukecyca/woodcut>
+Woodcut is a system for building static websites from Mako source files. It will walk your source directory, process any templates it finds, and produce a complete website in the build directory, ready to rsync to your webserver.
+
+For more information, visit <http://wiki.github.com/lukecyca/woodcut>.
--- setup.py
+++ setup.py
@@ -6,17 +6,19 @@ def read(fname):
 
 setup(
     name = "woodcut",
-    version = "0.3.0",
+    version = "0.4.0",
     author = "Luke Cyca",
     author_email = "me@lukecyca.com",
     description = ("Minimalist content management system for static websites."),
-    license = "BSD",
+    license = "BSD-new",
     keywords = "mako template static web development",
-    url = "https://github.com/lukecyca/woodcut",
+    url = "http://wiki.github.com/lukecyca/woodcut",
     long_description=read('README.markdown'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
         "Natural Language :: English",
         "Topic :: Internet :: WWW/HTTP :: Site Management",
         "Topic :: Text Processing :: Markup",
